b9219ced64fff8f79de8d009c45b216460da27ae
Pratham <prathamsinghal2003@gmail.com>
2022-12-02 23:36:24 +0530

    added header

---
+++ pages/brandhomepage.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/common/Header'
+const brandhomepage = () => {
+    return (
+        <div className="outer-bg">
+            <Header/>
+            
+            
+        </div>
+    );
+}
+
+export default brandhomepage;
