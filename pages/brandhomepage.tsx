bca35ab73bf93a2316a3d2aa963f16c8eb8d3701
Pratham <prathamsinghal2003@gmail.com>
2022-12-03 02:24:21 +0530

    added cards

--- pages/brandhomepage.tsx
+++ pages/brandhomepage.tsx
@@ -7,11 +7,19 @@ const brandhomepage = () => {
             <Header/>
             <Banner/>
             <h2 className='brand-hp-txt'>Availabe Campaigns for you</h2> 
-            <div>
+            <div className='cards-cont'>
+                <Cards img='assets/card-bg-1.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
+                <Cards img='assets/card-bg-2.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
+                <Cards img='assets/card-bg-3.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
+                <Cards img='assets/card-bg-1.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
                 <Cards img='assets/card-bg-1.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
             </div>
 
             <h2 className='brand-hp-txt'>My Collectables</h2> 
+            <div className='cards-cont'>
+                <Cards img='assets/card-bg-1.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event'/>
+                <Cards img='assets/card-bg-2.png' title='Christmas Gift' line1='Buy with $10 USD' />
+            </div>
         </div>
     );
 }
