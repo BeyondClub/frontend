1b8589aea8153b03f4e94c033318ebf1e3f8b284
Pratham <prathamsinghal2003@gmail.com>
2022-12-03 02:08:21 +0530

    added card template

--- pages/brandhomepage.tsx
+++ pages/brandhomepage.tsx
@@ -1,10 +1,17 @@
-import Header from '../components/common/Header'
+import Header from '../components/brandPage/Header'
+import Cards from '../components/brandPage/Cards'
+import Banner from '../components/brandPage/Banner'
 const brandhomepage = () => {
     return (
         <div className="outer-bg">
             <Header/>
-            
-            
+            <Banner/>
+            <h2 className='brand-hp-txt'>Availabe Campaigns for you</h2> 
+            <div>
+                <Cards img='assets/card-bg-1.png' title='Christmas Gift' line1='Buy with $10 USD' line2='Get the access of special Christmas event' time=' 5 days' redeemed=' 150' total='300'/>
+            </div>
+
+            <h2 className='brand-hp-txt'>My Collectables</h2> 
         </div>
     );
 }
