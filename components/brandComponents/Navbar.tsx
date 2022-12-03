7ed172546f5faf2bca666e223d828a1e3e624e3a
Pjijin <pjijin1@gmail.com>
2022-12-03 06:18:36 +0530

    🐛 Fix: custom details page

--- components/brandComponents/Navbar.tsx
+++ components/brandComponents/Navbar.tsx
@@ -1,19 +1,16 @@
-import Image from "next/image";
-import logo from "../../public/imgs/logo.svg";
+import Image from 'next/image';
+import logo from '../../public/imgs/logo.svg';
 
-import "./Navbar.scss";
+import './Navbar.scss';
 
 const LandingPage = () => {
-  return (
-      <div className="navbar">
-        <Image
-          src={logo}
-          alt="Picture of the author"
-          width={200}
-          height={300}
-        />
-        <button className="btn">Sign in</button>
-      </div>
-  );
+	return (
+		<div className="navbar bg-gray-50">
+			<Image src={logo} alt="" width={200} height={300} />
+			{/* <Link className="bg-gray-900 px-5 py-2  rounded-md text-gray-100" href="/register" passHref>
+				Sign in
+			</Link> */}
+		</div>
+	);
 };
 export default LandingPage;
