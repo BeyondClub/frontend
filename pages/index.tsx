6dd0844805e07b4faba9e0132fddec2747aeeca1
Pratham <prathamsinghal2003@gmail.com>
2022-12-03 10:55:41 +0530

    Updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home() {
 			<Navbar/>
 			<div className="modify grid grid-cols-2 gap-5 place-items-center pl-10">
 				<div className='z-50'>
-					<h2 className="z-50 text-6xl font-medium leading-40">
-						Supercharge your customer engagement with No-code NFT loyalty program.
+					<h2 className="z-50 text-6xl font-[550] leading-40 whitespace-nowrap">
+						Supercharge your <br/>customer engagement<br/> with No-code NFT <br/> loyalty program.
 					</h2>
 					<div>
 						<Link
