9edc79276e3ca48d91d3e11593d7fb61955f1045
akshaydhayal <akshaydhayalkd99@gmail.com>
2022-12-03 20:15:10 +0530

    Added ticket and video Modals to campaign

--- components/LivePeer/Player.tsx
+++ components/LivePeer/Player.tsx
@@ -1,35 +1,50 @@
-import { createReactClient, LivepeerConfig, studioProvider, ThemeConfig } from '@livepeer/react';
+import {
+  createReactClient,
+  LivepeerConfig,
+  studioProvider,
+  ThemeConfig,
+} from "@livepeer/react";
 
 const client = createReactClient({
-	provider: studioProvider({ apiKey: 'yourStudioApiKey' }),
+  provider: studioProvider({ apiKey: "yourStudioApiKey" }),
 });
 
 const livepeerTheme: ThemeConfig = {
-	colors: {
-		accent: 'rgb(0, 145, 255)',
-		containerBorderColor: 'rgba(0, 145, 255, 0.9)',
-	},
-	fonts: {
-		display: 'Inter',
-	},
+  colors: {
+    accent: "rgb(0, 145, 255)",
+    containerBorderColor: "rgba(0, 145, 255, 0.9)",
+  },
+  fonts: {
+    display: "Inter",
+  },
 };
 
-import { Player } from '@livepeer/react';
+import { Player } from "@livepeer/react";
 
-const playbackId = 'bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe';
+const playbackId =
+  "bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe";
 
 export function Livepeer() {
-	return <Player title="Waterfalls" playbackId={playbackId} loop autoPlay showTitle={false} muted />;
+  return (
+    <Player
+      title="Waterfalls"
+      playbackId={playbackId}
+      loop
+      autoPlay
+      showTitle={false}
+      muted
+    />
+  );
 }
 
 const VideoPlayer = () => {
-	return (
-		<>
-			<LivepeerConfig client={client} theme={livepeerTheme}>
-				<Livepeer />
-			</LivepeerConfig>
-		</>
-	);
+  return (
+    <>
+      <LivepeerConfig client={client} theme={livepeerTheme}>
+        <Livepeer />
+      </LivepeerConfig>
+    </>
+  );
 };
 
 export default VideoPlayer;
---
+++ components/brandPage/VideoModal.tsx
@@ -0,0 +1,53 @@
+import { Modal, BackgroundImage } from "@mantine/core";
+import Image from "next/image";
+import qr from "../../public/assets/qr.png";
+import VideoPlayer from "components/LivePeer/Player";
+
+function Demo(props) {
+  console.log(props);
+
+  return (
+    <Modal
+      size={530}
+      opened={props.handleTicketModal}
+      onClose={() => props.setHandleTicketModal(false)}
+      withCloseButton={false}
+    >
+      {/* Modal without header, presss escape or click on overlay to close */}
+      <div>
+        {/* w-80% bg-black" */}
+        <div className="">
+          <p className="text-black font-Helvetica text-xl font-bold  p-1 m-2">
+            You have an access for the special video!
+          </p>
+        </div>
+        <div className="">
+          {/* <h2 className="font-semibold font-serif m-1">
+            Christmas Special Event
+          </h2>
+          <div className="flex justify-between">
+            <div className="ticket-date">
+              <p className="text-gray-500">Date</p>
+              <p>Dec 24,2022 9pm-11pm</p>
+            </div>
+            <div className="ticket-loc">
+              <p className="text-gray-500">Venue</p>
+              <p>Central Park</p>
+            </div>
+          </div> */}
+          {/* <div className="ticket-qr">
+            <Image className="z-50" src={qr} alt="" width={300} height={330} />
+            <p className="text-gray-500 text-center">#150</p>
+            <VideoPlayer />
+            {/* <video autoPlay loop style={{ width: "500px", height: "500px" }}>
+              <source src="../../public/assets/vid.mp4" />
+            </video> */} 
+
+
+            <VideoPlayer />
+          </div>
+        </div>
+    </Modal>
+  );
+}
+export default Demo;
