a90fcf615d99e083d6be2c3cffae80c46e641087
Pratham <prathamsinghal2003@gmail.com>
2022-12-03 08:11:41 +0530

    updated campaign ui

--- pages/u/campaign/[id].tsx
+++ pages/u/campaign/[id].tsx
@@ -9,7 +9,7 @@ const Campaign = () => {
             <Banner img='/assets/banner-2.png'/>
             <div className='flex flex-row my-20 px-10'>
                 <div className='flex basis-3/4 justify-center'>
-                    <img className='rounded-md' src='/assets/card-bg-3.png' alt='campagin-img'/>
+                    <img className='rounded-md w-2/3 h-1/2' src='/assets/card-bg-3.png' alt='campagin-img'/>
                 </div>
                 <div>
                 <CampaignDetails title='Christmas Gift Campaign' desc='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?' duration='2022/12/01 0:00 - 2022/12/24 10:00 UTC' howToGet='You can buy it for $10 USD' whatToGet='Lorem ipsum dolor sit amet, consectetur adipisicing elit' claimablePeriod='2022/12/01 0:00 - 2022/12/24 10:00 UTC' redeemed='150' total='300'/>
