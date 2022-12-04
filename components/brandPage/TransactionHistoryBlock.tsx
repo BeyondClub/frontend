de4c11d7d19048ba9ca9703edd7b6f2c975697ac
Pratham <prathamsinghal2003@gmail.com>
2022-12-04 06:20:58 +0530

    updated transaction history modal

--- components/brandPage/TransactionHistoryBlock.tsx
+++ components/brandPage/TransactionHistoryBlock.tsx
@@ -1,17 +1,21 @@
 const TransactionHistoryBlock = (props) => {
     return (
-        <div className="flex flex-col my-23 bg-slate-200 w-full rounded ">
+        <div className="flex flex-col my-23 bg-slate-200 w-full rounded my-2 p-3">
             <div className='flex flex-row'>
                 <h2 className='font-[600]'>At :</h2>
-                <h2>{props.at}</h2>
+                <h2 className='mx-2'>{props.at}</h2>
             </div>
             <div className='flex flex-row'>
                 <h2 className='font-[600]'>To :</h2>
-                <h2>{props.to}</h2>
+                <h2 className='mx-2'>{props.to}</h2>
             </div>
             <div className='flex flex-row'>
                 <h2 className='font-[600]'>Height :</h2>
-                <h2>{props.height}</h2>
+                <h2 className='mx-2'>{props.height}</h2>
+            </div>
+            <div className='flex flex-row'>
+                <h2 className='font-[600]'>Fees :</h2>
+                <h2 className='mx-2'>{props.fees*(10**-18)}</h2>
             </div>
         </div>
     );
--- components/brandPage/TransactionsFetch.tsx
+++ components/brandPage/TransactionsFetch.tsx
@@ -2,21 +2,21 @@ import { Modal,Loader } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import TransactionHistoryBlock from './TransactionHistoryBlock';
 function TransactionModal(props) {
+    // var data;
     const [loading, setLoading] = useState(false);
     const [txnData, setTxnData] = useState();
     useEffect(() => {
-    {props.txnHash? fetchData():setTxnData("Transaction Hash cant be Empty")};
-    // setLoading(false);
+    {props.txnHash? fetchData():null};
+        // setLoading(false);
     }, [])
         const fetchData = async () => {
         setLoading(true)
-        console.log(loading)
       const response=await fetch(
-        // `https://api.covalenthq.com/v1/${props.chainId}/transaction_v2/{${props.txnHash}}/?key=$`
         `https://api.covalenthq.com/v1/${props.chainId}/transaction_v2/`+props.txnHash+`/?key=${process.env.COVALENTHQ_API_KEY}`
         );
-        const data = response.json()
-        console.log('Data is',data);
+        let data = await response.json()
+        console.log('Data is',data.data.items[0]);
+        setTxnData(data.data)
         setLoading(false)
     }
   return (
@@ -24,14 +24,14 @@ function TransactionModal(props) {
     opened={props.opened}
     onClose={() => props.setOpened(false)} 
     withCloseButton={false}
-    size='50%'>
+    size='40%'>
         <div className='text-center'>
         <h2 className='font-[650] text-3xl border-b-2 my-1 py-2 border-grey-100'>Transaction History</h2>
         </div>
-    {loading? <div className='flex w-full justify-center'><Loader className='my-10'/></div>: 
+    {(!txnData)? <div className='flex w-full justify-center'><Loader className='my-10'/></div>: 
     <div> 
-        <TransactionHistoryBlock to='0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb4' at='2022-12-03T18:47:12Z' height='16106165' />
-        <TransactionHistoryBlock to='0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb4' at='2022-12-03T18:47:12Z' height='16106165' />
+        <TransactionHistoryBlock to={txnData.items[0]['to_address']} at={txnData.items[0]['block_signed_at']} height={txnData.items[0]['block_height']} fees={txnData.items[0]['fees_paid']
+} />
     </div>}
     </Modal>
   );
